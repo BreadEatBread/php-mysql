83163e6cb15609fbf2abcde94760f17a2994c4da
BreadEatBread <woong4252@gmail.com>
2022-10-05 16:18:47 +0900

    10.05.22 16:20

--- chapter4/process_create.php
+++ chapter4/process_create.php
@@ -1,5 +1,5 @@
 <?php
-    // $conn = mysqli_connect("localhost", "root", "koreanit", "tutorials");
+    $conn = mysqli_connect("localhost", "root", "koreanit", "tutorials");
     $sql = "
         INSERT INTO topic 
         (title, description, created) 
@@ -9,12 +9,12 @@
             NOW()
         )
     ";
-    // $result = mysqli_query($conn, $sql);
-    // if( $result == false) {
-    //     echo '저장하는 과정에서 문제가 생겼습니다. 관리자에게 문의해주세요';
-    //     error_log(mysqli_error($conn));
-    // } else {
-    //     echo '성공했습니다. <a href="index.php">돌아가기</a>';
-    // }
+    $result = mysqli_query($conn, $sql);
+    if( $result == false) {
+        echo '저장하는 과정에서 문제가 생겼습니다. 관리자에게 문의해주세요';
+        error_log(mysqli_error($conn));
+    } else {
+        echo '성공했습니다. <a href="index.php">돌아가기</a>';
+    }
     echo $sql;
 ?>
