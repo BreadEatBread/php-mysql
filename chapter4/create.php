370e9e2cf5b0b37e09b58f2d7f702af3dcc519ac
BreadEatBread <woong4252@gmail.com>
2022-10-05 13:36:06 +0900

    10.05.22 13:30

--- chapter4/create.php
+++ chapter4/create.php
@@ -2,8 +2,12 @@
     require('lib/print.php');
     require('view/top.php');
 ?>
-        <a href="create.php">create</a>
-        <form action="create_process.php" method="post">
+        <form action = "process_create.php" method = "post">
+            <p><input type = "text" name = "title" placeholder = "title"></p>
+            <p><textarea name = "decription" placeholder = "description"></textarea></p>
+            <p><input type = "submit"></p>
+        </form>
+        <!-- <form action="create_process.php" method="post">
             <p>
                 <input type="text" name="title" placeholder="Title" />
             </p>
@@ -13,7 +17,7 @@
             <p>
                 <input type="submit" value="Submit" />
             </p>
-        </form>
+        </form> -->
         <h2>
             <?php
                print_title();    
--- chapter4/insert.php
+++ chapter4/insert.php
@@ -1,10 +1,17 @@
 <?php
-    $mysqli = mysqli_connect("localhost", "root", "koreanit", "tutorials");
-    $res = mysqli_query($mysqli,"SHOW TABLES FROM tutorials;");
-    if(!$res) {
-        echo mysqli_error($mysqli);
+    $conn = mysqli_connect("localhost", "root", "koreanit", "tutorials");
+    $sql = "
+        INSERT INTO topic (
+        title,
+        description,
+        created
+        ) VALUES (
+            'MySQL',
+            'MySQL is ....',
+            NOW()
+        )";
+    $result = mysqli_query($conn, $sql);
+    if( $result == false ) {
+        echo mysqli_error($conn);
     }
-
-    $row = mysqli_fetch_row($res);
-    var_dump($row);
 ?>
